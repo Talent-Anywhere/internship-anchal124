48680b74c13fa802253fe18f8d26c9f52f12cefa
anchal155 <anchal.somani05@gmail.com>
2021-02-24 13:29:17 +0530

    my-CI-APP

---
+++ application/views/Test.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> 
+<html lang = "en"> 
+
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>Login Form</title> 
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <link rel = "stylesheet" type = "text/css" 
+   href = "<?php echo base_url(); ?>css/style.css">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	
+
+   </head>
+	
+   <body> 
+      <div class="login-form">
+    <form action="/examples/actions/confirmation.php" method="post">
+        <h2 class="text-center">Log in</h2>       
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Username" required="required">
+        </div>
+        <div class="form-group">
+            <input type="password" class="form-control" placeholder="Password" required="required">
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block">Log in</button>
+        </div>
+        <div class="clearfix">
+            <label class="float-left form-check-label"><input type="checkbox"> Remember me</label>
+            <a href="#" class="float-right">Forgot Password?</a>
+        </div>        
+    </form>
+    <p class="text-center"><a href="#">Create an Account</a></p>
+	</div> 
+   </body>
+	
+</html>
